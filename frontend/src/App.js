975c9d9b2155e0237b03a6bc1b11c1d1cf8ed57b
desivar <jillian_desire@yahoo.es>
2025-12-22 17:58:55 -0600

    frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
 import SearchPage from './pages/SearchPage';
+import UploadPage from './pages/UploadPage'; // New Import
 
 function App() {
   return (
-    <div className="App">
-      <SearchPage />
-    </div>
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+        <div style={{ marginLeft: '260px', flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<SearchPage />} />
+            <Route path="/upload" element={<UploadPage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
