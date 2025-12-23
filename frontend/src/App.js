6831dd64d53c925b84f257f9f5e40663b769496f
desivar <jillian_desire@yahoo.es>
2025-12-22 18:20:08 -0600

    updated  frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import SearchPage from './pages/SearchPage';
-import UploadPage from './pages/UploadPage'; // New Import
+import UploadPage from './pages/UploadPage';
+import CollectionView from './pages/CollectionView'; // Import the missing view
 
 function App() {
   return (
@@ -11,8 +12,14 @@ function App() {
         <Sidebar />
         <div style={{ marginLeft: '260px', flex: 1 }}>
           <Routes>
+            {/* Main Pages */}
             <Route path="/" element={<SearchPage />} />
             <Route path="/upload" element={<UploadPage />} />
+
+            {/* Collection & Index Filters */}
+            <Route path="/category/:value" element={<CollectionView type="category" />} />
+            <Route path="/type/:value" element={<CollectionView type="recordType" />} />
+            <Route path="/alpha/:value" element={<CollectionView type="letter" />} />
           </Routes>
         </div>
       </div>
