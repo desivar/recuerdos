c4e67a913e5cb5cf78be21658fc29bce51aa59c5
desivar <jillian_desire@yahoo.es>
2025-12-31 14:02:16 -0600

    change from local to Render

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -15,7 +15,8 @@ const SearchPage = () => {
   const fetchAllRecords = async () => {
     setLoading(true);
     try {
-      const response = await axios.get(`http://localhost:5000/api/search`);
+      // Changed from localhost to your live Render Backend URL
+      const response = await axios.get(`https://recuerdos-4lm3.onrender.com/api/search`);
       setResults(response.data);
     } catch (error) {
       console.error("Error loading archive:", error);
